8d9c80d1c9b34226a5cb1ae87d9fc68b7c3c5418
helmigandi <sugandihelmi@gmail.com>
2025-05-08 08:18:53 +0700

    Add service logic

---
+++ service/category_service_impl.go
@@ -0,0 +1,92 @@
+package service
+
+import (
+	"context"
+	"database/sql"
+	"github.com/go-playground/validator/v10"
+	"github.com/helmigandi/go-restapi-1/helper"
+	"github.com/helmigandi/go-restapi-1/model/domain"
+	"github.com/helmigandi/go-restapi-1/model/web"
+	"github.com/helmigandi/go-restapi-1/repository"
+)
+
+type CategoryServiceImpl struct {
+	CategoryRepository repository.CategoryRepository
+	DB                 *sql.DB
+	validate           *validator.Validate
+}
+
+func NewCategoryService(categoryRepository repository.CategoryRepository, db *sql.DB, validate *validator.Validate) CategoryService {
+	return &CategoryServiceImpl{
+		CategoryRepository: categoryRepository,
+		DB:                 db,
+		validate:           validate,
+	}
+}
+
+func (service *CategoryServiceImpl) Create(ctx context.Context, request web.CategoryCreateRequest) web.CategoryResponse {
+	err := service.validate.Struct(request)
+	helper.PanicIfError(err)
+
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	category := domain.Category{
+		Name: request.Name,
+	}
+
+	category = service.CategoryRepository.Save(ctx, tx, category)
+
+	return helper.ToCategoryResponse(category)
+}
+
+func (service *CategoryServiceImpl) Update(ctx context.Context, request web.CategoryUpdateRequest) web.CategoryResponse {
+	err := service.validate.Struct(request)
+	helper.PanicIfError(err)
+
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	category, err := service.CategoryRepository.FindById(ctx, tx, request.Id)
+	helper.PanicIfError(err)
+
+	category.Name = request.Name
+
+	service.CategoryRepository.Update(ctx, tx, category)
+
+	return helper.ToCategoryResponse(category)
+}
+
+func (service *CategoryServiceImpl) Delete(ctx context.Context, categoryId int) {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	category, err := service.CategoryRepository.FindById(ctx, tx, categoryId)
+	helper.PanicIfError(err)
+
+	service.CategoryRepository.Save(ctx, tx, category)
+}
+
+func (service *CategoryServiceImpl) FindById(ctx context.Context, categoryId int) web.CategoryResponse {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	category, err := service.CategoryRepository.FindById(ctx, tx, categoryId)
+	helper.PanicIfError(err)
+
+	return helper.ToCategoryResponse(category)
+}
+
+func (service *CategoryServiceImpl) FindAll(ctx context.Context) []web.CategoryResponse {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	categories := service.CategoryRepository.FindAll(ctx, tx)
+
+	return helper.ToCategoryResponses(categories)
+}
